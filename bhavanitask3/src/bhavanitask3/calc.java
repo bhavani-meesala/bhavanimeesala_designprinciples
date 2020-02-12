86190800131446e8548ff1c703d6dfb292686446
bhavani-meesala <59745309+bhavani-meesala@users.noreply.github.com>
2020-02-12 22:15:45 +0530

    Add files via upload

---
+++ bhavanitask3/src/bhavanitask3/calc.java
@@ -0,0 +1,9 @@
+package bhavanitask3;
+
+public class calc {
+	int add(int a,int b) {return a+b;}
+	int sub(int a,int b) {return a-b;}
+	int mul(int a,int b) {return a*b;}
+	float div(int a,int b) {return (float)a/(float) b;}
+
+}
